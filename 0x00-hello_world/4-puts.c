7c4ddf05c0d79fc41a14b2c3541405e33b18e957
Your Name <snowzyember@gmail.com>
2023-02-16 07:11:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline,"
+ * return 0.
+ */
+
+int main(void)
+
+{
+	put("\"programming is like building a multilingual puzzle");
+
+	return(0);
+}
